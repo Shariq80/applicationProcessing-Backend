1e47a128f6ebafb6dd97c2f384fc0cea066953c2
Shariq80 <cse.160320733080@gmail.com>
2024-10-20 14:50:48 +0530

    Implement Process Emails

--- backend/routes/applicationRoutes.js
+++ backend/routes/applicationRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
 const { getApplications, updateApplicationStatus } = require('../controllers/applicationController');
+const { fetchAndProcessEmails } = require('../controllers/emailController');
 
 const router = express.Router();
 
 router.get('/:jobId', protect, getApplications);
 router.put('/:id/status', protect, updateApplicationStatus);
+router.post('/:jobId/process-emails', protect, fetchAndProcessEmails);
 
 module.exports = router;
