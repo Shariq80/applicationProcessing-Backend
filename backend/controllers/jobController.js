1c28f1de331c2d7a23bdaa09d09d444a2bb2afb2
Shariq80 <cse.160320733080@gmail.com>
2024-10-25 09:50:40 +0530

    Updated repo

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -1,5 +1,6 @@
 const Job = require('../models/Job');
 const User = require('../models/User');
+const Application = require('../models/Application');
 
 const createJob = async (req, res) => {
   try {
@@ -50,13 +51,22 @@ const updateJob = async (req, res) => {
 const deleteJob = async (req, res) => {
   try {
     const { id } = req.params;
+
+    // Delete associated applications
+    await Application.deleteMany({ job: id });
+
+    // Delete the job
     const job = await Job.findOneAndDelete({ _id: id, createdBy: req.user._id });
     if (!job) {
       return res.status(404).json({ message: 'Job not found' });
     }
-    res.json({ message: 'Job deleted successfully' });
+
+    // Remove job from user's jobs array
+    await User.findByIdAndUpdate(req.user._id, { $pull: { jobs: id } });
+
+    res.json({ message: 'Job and associated applications deleted successfully' });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: 'Error deleting job', error: error.message });
   }
 };
 
